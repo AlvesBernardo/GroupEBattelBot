537e2e39eed68056e270dc40cdfa7e71d6af7d2c
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2023-02-06 14:55:30 +0100

    Add: Funtion for distance

--- exercice1/exercice1.ino
+++ exercice1/exercice1.ino
@@ -17,33 +17,37 @@ int motorPin4 = 2;
 // defines variables
 long duration;
 int distance;
-int PIN = 7;
+int neoPIN = 7; //NEO pixel datapin
 int NUMPIXELS = 4;
 
 //motors
-  ;  //initializing pin 2 as pwm
-const int in_A0 = A0 ;
-const int in_A1 = A1 ;
-const int in_A2 = A2 ;
-const int in_A3 = A4 ;
 
+const int in_A1 = A0 ;
+const int in_A2 = A1 ;
+const int in_A3 = A2 ;
+const int in_A4 = A3 ;
 
+//optional buttons to simulate a car starting
+const int buttonPin = 2;
+int buttonState = 0;
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUMPIXELS, neoPIN, NEO_GRB + NEO_KHZ800);
 #define DELAYVAL 500 
 
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600); // Starts the serial communication
-  pinMode(PIN, OUTPUT);
-   pixels.begin();
+  pinMode(neoPIN, OUTPUT); // neo pixel as output
+   pixels.begin();         //begin it
   pixels.setBrightness(50);
   pixels.show(); // Initialize all pixels to 'off'
   pinMode(in_A1,OUTPUT);
-  pinMode(in_A3,OUTPUT);
+  pinMode(in_A4,OUTPUT);
+
   
- 
+ //button declaration
+ pinMode(buttonPin,INPUT);
  
 }
 
@@ -51,52 +55,67 @@ void setup() {
 
 
 void loop() {
+  
+   buttonState = digitalRead(buttonPin);
+ 
+
+      // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance = duration * 0.034 / 2;
+    // Prints the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    digitalWrite(motorPin, HIGH);
+  
+  
+   
+ detectDistance();
+
 
     
-    // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  digitalWrite(motorPin, HIGH);
 
-
- 
-if (distance > 10){
-
-     pixels.begin();
-    pixels.setBrightness(50);
-    pixels.show(); // Initialize all pixels to 'off'
-    Serial.print("no hand");
-    analogWrite(in_A1,255);
-     analogWrite(in_A3,255);
-    }
-  
-if (distance < 10){
-        
-      for(int i=0; i<NUMPIXELS; i++) {
-          pixels.setPixelColor(i,pixels.Color(0, 0, 255));
-            pixels.begin();
-             Serial.print("hand");
-           analogWrite(in_A1,0);
-            analogWrite(in_A3,0);
-      }
-  }
-
-
-
-    //motors
 
 }
+
+
+
+/*    ======[funtions] ========*/
+
+
+
+void detectDistance(){
+
+  if (distance > 25){
+        //if distance above 10 neopixel off
+        for(int i=0; i<NUMPIXELS; i++) {
+            pixels.setPixelColor(i,pixels.Color(137, 207, 240));
+              pixels.show();
+                analogWrite(in_A2,255);
+                analogWrite(in_A4,255);
+             
+        }
+    
+      }else {
+              //distance less then 10 neopixel on
+        for(int i=0; i<NUMPIXELS; i++) {
+            pixels.setPixelColor(i,pixels.Color(255, 0, 0));
+              pixels.show();
+              analogWrite(in_A2,0);
+                analogWrite(in_A4,0);
+        }
+      }
+
+
+  
+  }
 
 
 

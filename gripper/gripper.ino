50bd41af21061cdf91ff70a6cf05510354962971
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-02-09 11:07:45 +0100

    Add: gripper code

--- gripper/gripper.ino
+++ gripper/gripper.ino
@@ -1,5 +1,7 @@
+#include <Servo.h>
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
+
 
 //defining pins
 const int trig = 12;
@@ -7,9 +9,9 @@ const int echo = 13;
 
 //motor pins
 const int motorPin1 = 6;
-const int motorPin1 = 9;
-const int motorPin1 = 10;
-const int motorPin1 = 11;
+const int motorPin2 = 9;
+const int motorPin3 = 10;
+const int motorPin4 = 11;
 
 // line sensor pins
 const int lineSensor1 = A0;
@@ -22,16 +24,34 @@ const int lineSensor7 = A6;
 const int lineSensor8 = A7;
 
 // other pins
-const int gripper = 5;
+const int gripper = 8;
 const int neoPix = 7;
 
+// create servo object
+
+Servo servo;
+int angle = 10;
 
 void setup() {
   // put your setup code here, to run once:
+pinMode(gripper,OUTPUT);
+
+//attach servo object
+
+servo.attach(gripper);
+servo.write(angle);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+ // scan from 0 to 180 degrees
 
+  for (angle = 10; angle < 180; angle++)
+  {
+    servo.write(angle);
+    delay(15);
+  }
+  
 }

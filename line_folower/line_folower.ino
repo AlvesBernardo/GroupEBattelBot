c99c01b1b67d48cf458bd3f760b583516753e665
AlexMednov <alex.mednov@student.nhlstenden.com>
2023-02-22 10:31:02 +0100

    The robot stops at an object, tested. Evasion implementation pending

--- line_folower/line_folower.ino
+++ line_folower/line_folower.ino
@@ -3,28 +3,27 @@
 *************************/
   
 //==================[ DEFINITION OF PINS ]====================
-  const int PinLight0=A0;
-  const int PinLight1=A1;
-  const int PinLight2=A2;
-  const int PinLight3=A3;
-  const int PinLight4=A4;
-  const int PinLight5=A5;
-  const int PinLight6=A6;
-  const int PinLight7=A7;
+const int trunRatio = 25;
+const int baseSpeed = 250;
+const int minSpeed = 75;
 
-  const int trunRatio = 25;
-  const int baseSpeed = 250;
-  const int minSpeed = 75;
+const int motorBPin2 = 6; //left wheel backward
+const int motorBPin1 = 9; //left wheel forward
+const int motorAPin2 = 10; //right wheel forward
+const int motorAPin1 = 11; //right wheel backwards
 
-  int motorBPin2 = 6; //left wheel backward
-  int motorBPin1 = 9; //left wheel forward
-  int motorAPin2 = 10; //right wheel forward
-  int motorAPin1 = 11; //right wheel backwards
+int pinArr[]={A7, A6, A5, A4, A3, A2, A1, A0};//initialisation of pins
+int posVal[]={-4,-3,-2,-1,1,2,3,4};//possible values
 
-  int pinArr[]={A7, A6, A5, A4, A3, A2, A1, A0};//initialisation of pins
-  int posVal[]={-4,-3,-2,-1,1,2,3,4};//possible values
+int position = 0;
 
-  int position = 0;
+
+
+const int trig = 4;
+const int echo = 2;
+
+long duration;
+int distance;
 
 //==================[ SETUP ]==================================
 void setup() {
@@ -35,47 +34,35 @@ void setup() {
   pinMode(motorBPin1, OUTPUT);
   pinMode(motorAPin1, OUTPUT);
   pinMode(motorAPin2, OUTPUT);
-
+  
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
 }
 //=====================[ LOOP BEGINING ] =======================
 int sensRead[8];//empty array for reading results
 void loop() {
-  for(int i = 0; i<8; i++){
-    if(analogRead(pinArr[i])>750){
-      sensRead[i]=1;
-    }
-    else if(analogRead(pinArr[i])<500){
-      sensRead[i]=0;
-    }
-  }//the sensors reading are stored in previously empty array
-  /*
-  for(int i=0; i<8; i++){
-    Serial.print(sensRead[i]);
-    Serial.print("-");
-  }
-  Serial.println("-");
-  /*
-  Serial.println("-");
-  if(sensRead[1]==0){
-    goForward();
+  if(detectObject()){
+        stop();    
   }
   else{
-    stop();
-  }*/
-
-  int position = conversion(sensRead);//conversts reading to position
-  Serial.println(position);
-  move(position);//move
+    for(int i = 0; i<8; i++){
+      if(analogRead(pinArr[i])>750){
+        sensRead[i]=1;
+      }
+      else if(analogRead(pinArr[i])<750){
+        sensRead[i]=0;
+      }
+    }//the sensors reading are stored in previously empty array
+    int position = conversion(sensRead);//conversts reading to position
+    Serial.println(position);
+    move(position);//move
+  }
 }
 //=========================[ END OF PROGGRAM]=======================
 
 
 
 //========================[ FUNCTIONS ]===============================
-void goForward(){
-  analogWrite(motorBPin1, 150);
-  analogWrite(motorAPin2, 150);
-}
 void stop(){
   analogWrite(motorBPin1, 0);
   analogWrite(motorAPin2, 0);
@@ -105,16 +92,33 @@ void move(int position){//movement based on the reading of position
   }
   if(position>0){
     analogWrite(motorAPin2, baseSpeed-(trunRatio*position));
+    analogWrite(motorBPin1, baseSpeed);
   }
   if(position<0){
-    analogWrite(motorBPin1, baseSpeed+(trunRatio*position));    
-  }
-  if(position>6){
-    analogWrite(motorBPin1, baseSpeed);
-    analogWrite(motorAPin2, minSpeed);
-  }
-  if(position<(-7)){
-    analogWrite(motorBPin1, minSpeed);
+    analogWrite(motorBPin1, baseSpeed+(trunRatio*position));
     analogWrite(motorAPin2, baseSpeed);
   }
+}
+
+bool detectObject(){
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+
+
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(5);
+  //max 200cm
+  digitalWrite(trig, LOW);
+
+
+  duration = pulseIn(echo, HIGH);
+  distance = duration*0.0344/2;
+  if(distance<20){
+    return true;
+  }
+  return false;
+}
+
+void evade(){
+
 }

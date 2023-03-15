41a667538b647fe8b792826fc65ea7b53b149f9c
AlexMednov <alex.mednov@student.nhlstenden.com>
2023-03-15 12:55:20 +0100

    Implemented lights; implemented failsave; 10 tests in a row, no derailment

--- line_folower/line_folower.ino
+++ line_folower/line_folower.ino
@@ -18,17 +18,19 @@ int pinArr[]={A7, A6, A5, A4, A3, A2, A1, A0};//initialisation of pins
 int posVal[]={-4,-3,-2,-1,1,2,3,4};//possible values
 
 int position = 0;
+
 const int maxIntercectionDuration = 10;//mesuring unit used to stop, the smaller it is the faster it stops. Make it too small it will stop at the intersections.
 int curentIntercectionDuration = 0;
 
 int neoPin = 7;//neopixels declaration
 int NUMPIXELS = 4;
-const int GRBdef = {171, 140, 153};
-const int GRBforward = {255, 0, 0};
-const int GRBturn = {165, 255, 0};
-const int GRBextremeTurn = {0, 255, 255};
-const int GRBstop = {0, 255, 0};
-const int GRBevsasion = {0, 0, 255};
+Adafruit_NeoPixel pixels(NUMPIXELS, neoPin, NEO_GRB + NEO_KHZ800);
+const int GRBdef[] = {171, 140, 153};
+const int GRBforward[] = {255, 0, 0};
+const int GRBturn[] = {165, 255, 0};
+const int GRBextremeTurn[] = {0, 255, 255};
+const int GRBstop[] = {0, 255, 0};
+const int GRBevsasion[] = {0, 0, 255};
 
 
 const int trig = 4;
@@ -36,6 +38,8 @@ const int echo = 2;
 
 long duration;
 int distance;
+
+bool isRaceStarted = false;//switchable for testing
 
 //==================[ SETUP ]==================================
 void setup() {
@@ -53,44 +57,50 @@ void setup() {
   pinMode(neoPin,OUTPUT);
   pixels.begin();
   pixels.setBrightness(50);
-
-  startRace();
+  for(int i=0; i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(GRBdef[0], GRBdef[1], GRBdef[2]));
+  }
+  pixels.show();
 }
 //=====================[ LOOP BEGINING ] =======================
 int sensRead[8];//empty array for reading results
 void loop() {
-  if(detectObject()){
-        evade();    
-  }
-  else{
-    for(int i = 0; i<8; i++){
-      if(analogRead(pinArr[i])>750){
-        sensRead[i]=1;
-      }
-      else if(analogRead(pinArr[i])<750){
-        sensRead[i]=0;
-      }
-    }//the sensors reading are stored in previously empty array
-    
-    int position = conversion(sensRead);//conversts reading to position
-
-    if(isIntercection(sensRead)){
-      curentIntercectionDuration++;
-      Serial.println(curentIntercectionDuration);
-            
+  if(isRaceStarted){
+    if(detectObject()){
+          evade();    
     }
     else{
-      curentIntercectionDuration = 0;
-    }
-    if(curentIntercectionDuration>maxIntercectionDuration){
-      stop();
-      while(true){
-        delay(1);
+      for(int i = 0; i<8; i++){
+        if(analogRead(pinArr[i])>750){
+          sensRead[i]=1;
+        }
+        else if(analogRead(pinArr[i])<750){
+          sensRead[i]=0;
+        }
+      }//the sensors reading are stored in previously empty array
+      
+      int position = conversion(sensRead);//conversts reading to position
+
+      if(isIntercection(sensRead)){
+        curentIntercectionDuration++;
       }
+      else{
+        curentIntercectionDuration = 0;
+      }
+      if(curentIntercectionDuration>maxIntercectionDuration){
+        stop();
+        while(true){
+          delay(1);
+        }
+      }
+      //Serial.println(curentIntercectionDuration);
+      //Serial.println(position);
+      move(position, sensRead);//move
     }
-    //Serial.println(curentIntercectionDuration);
-    //Serial.println(position);
-    move(position);//move
+  }
+  else{
+    startRace();
+    isRaceStarted=true;    
   }
 }
 //=========================[ END OF PROGGRAM]=======================
@@ -103,6 +113,11 @@ void stop(){
   analogWrite(motorAPin2, 0);
   analogWrite(motorAPin1, 0);
   analogWrite(motorBPin2, 0);
+
+  for(int i=0; i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(GRBstop[0], GRBstop[1], GRBstop[2]));
+  }
+  pixels.show();
 }
 int conversion(int reading[]){//first of all it checks for outliers such as 10000000 and 00000001;
   int position = 0;
@@ -122,12 +137,23 @@ int conversion(int reading[]){//first of all it checks for outliers such as 1000
   return position;//returns position
 }
 
-void move(int position){//movement based on the reading of position
+void move(int position, int reading[]){//movement based on the reading of position
+  if(reading[0]==0 && reading[1]==0 && reading[2]==0 && reading[3]==0 && reading[4]==0 && reading[5]==0 && reading[6]==0 && reading[7]==0){
+    delay(100);
+    return;
+  }
   if(position == 0){
     analogWrite(motorAPin1, 0);
     analogWrite(motorBPin2, 0);
     analogWrite(motorBPin1, baseSpeed);
-    analogWrite(motorAPin2, baseSpeed);    
+    analogWrite(motorAPin2, baseSpeed);
+
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+    pixels.setPixelColor(2, pixels.Color(GRBforward[0], GRBforward[1], GRBforward[2]));
+    pixels.setPixelColor(3, pixels.Color(GRBforward[0], GRBforward[1], GRBforward[2]));
+    pixels.show();
+
   }
   if(position>0 && position<12){
     if(trunRatio*position<255){
@@ -135,12 +161,24 @@ void move(int position){//movement based on the reading of position
       analogWrite(motorBPin2, 0);
       analogWrite(motorAPin2, baseSpeed-(trunRatio*position));
       analogWrite(motorBPin1, baseSpeed);
+
+      pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+      pixels.setPixelColor(1, pixels.Color(GRBturn[0], GRBturn[1], GRBturn[2]));
+      pixels.setPixelColor(2, pixels.Color(GRBturn[0], GRBturn[1], GRBturn[2]));
+      pixels.setPixelColor(3, pixels.Color(0, 0, 0));
+      pixels.show();
     }
     else{
       analogWrite(motorAPin1, 0);
       analogWrite(motorBPin2, 0);
       analogWrite(motorAPin2, 0);
       analogWrite(motorBPin1, baseSpeed);
+
+      pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+      pixels.setPixelColor(1, pixels.Color(GRBturn[0], GRBturn[1], GRBturn[2]));
+      pixels.setPixelColor(2, pixels.Color(GRBturn[0], GRBturn[1], GRBturn[2]));
+      pixels.setPixelColor(3, pixels.Color(0, 0, 0));
+      pixels.show();
     }
   }
   if(position<0 && position>-12){
@@ -149,12 +187,24 @@ void move(int position){//movement based on the reading of position
       analogWrite(motorBPin2, 0);
       analogWrite(motorBPin1, baseSpeed+(trunRatio*position));
       analogWrite(motorAPin2, baseSpeed);
+
+      pixels.setPixelColor(0, pixels.Color(GRBturn[0], GRBturn[1], GRBturn[2]));
+      pixels.setPixelColor(1, pixels.Color(0,0,0));
+      pixels.setPixelColor(2, pixels.Color(0,0,0));
+      pixels.setPixelColor(3, pixels.Color(GRBturn[0], GRBturn[1], GRBturn[2]));
+      pixels.show();
     }
     else{
       analogWrite(motorAPin1, 0);
       analogWrite(motorBPin2, 0);
       analogWrite(motorBPin1, 0);
       analogWrite(motorAPin2, baseSpeed);
+
+      pixels.setPixelColor(0, pixels.Color(GRBturn[0], GRBturn[1], GRBturn[2]));
+      pixels.setPixelColor(1, pixels.Color(0,0,0));
+      pixels.setPixelColor(2, pixels.Color(0,0,0));
+      pixels.setPixelColor(3, pixels.Color(GRBturn[0], GRBturn[1], GRBturn[2]));
+      pixels.show();
     }
   }
   if(position>12){
@@ -162,12 +212,24 @@ void move(int position){//movement based on the reading of position
     analogWrite(motorBPin2, 0);
     analogWrite(motorAPin2, 0);
     analogWrite(motorBPin1, baseSpeed);
+
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.setPixelColor(1, pixels.Color(GRBextremeTurn[0], GRBextremeTurn[1], GRBextremeTurn[2]));
+    pixels.setPixelColor(2, pixels.Color(GRBextremeTurn[0], GRBextremeTurn[1], GRBextremeTurn[2]));
+    pixels.setPixelColor(3, pixels.Color(0, 0, 0));
+    pixels.show();
   }
   if(position<-12){
     analogWrite(motorAPin1, 0);
     analogWrite(motorBPin2, baseSpeed);
     analogWrite(motorAPin2, baseSpeed);
     analogWrite(motorBPin1, 0);
+
+    pixels.setPixelColor(0, pixels.Color(GRBextremeTurn[0], GRBextremeTurn[1], GRBextremeTurn[2]));
+    pixels.setPixelColor(1, pixels.Color(0,0,0));
+    pixels.setPixelColor(2, pixels.Color(0,0,0));
+    pixels.setPixelColor(3, pixels.Color(GRBextremeTurn[0], GRBextremeTurn[1], GRBextremeTurn[2]));
+    pixels.show();
   }
 }
 
@@ -191,6 +253,13 @@ bool detectObject(){
 void evade(){
   if(detectObject()){
     stop();
+
+    pixels.setPixelColor(0, pixels.Color(GRBevsasion[0], GRBevsasion[1], GRBevsasion[2]));//
+    pixels.setPixelColor(1, pixels.Color(GRBevsasion[0], GRBevsasion[1], GRBevsasion[2]));
+    pixels.setPixelColor(2, pixels.Color(GRBevsasion[0], GRBevsasion[1], GRBevsasion[2]));
+    pixels.setPixelColor(3, pixels.Color(GRBevsasion[0], GRBevsasion[1], GRBevsasion[2]));//
+    pixels.show();
+    
     analogWrite(motorBPin1, baseSpeed);
     delay(300);
     analogWrite(motorBPin1, baseSpeed);

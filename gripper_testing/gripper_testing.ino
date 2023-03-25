a41ab18a8da6ea0183233b62f74af4957a6a4bc5
AlexMednov <alex.mednov@student.nhlstenden.com>
2023-03-25 22:28:11 +0100

    Gripper is now working, implementation to the main file pending

--- gripper_testing/gripper_testing.ino
+++ gripper_testing/gripper_testing.ino
@@ -1,39 +1,30 @@
 const int gripperPin = 13;
-int servoMinPulse = 500;
-int servoCenterPulse = 1400; // 
-int servoMaxPulse = 2400; //in microseconds
-
-int gripper_open_pulse = 2100;
-int gripper_close_pulse =  800; // 
-int servoPulseRepeat = 10; // number of pulse send to servo
 
 void setup() {
   pinMode(gripperPin, OUTPUT);
 }
 
 void loop() {
-    //openGripper();
-    //delay(10000);
-    //closeGripper();
-    //min pulse for closing
-
-    //closeGripper();
-    //delay(5000);
-    //servo(gripperPin, servoMaxPulse);
-}
-
-
-void servo(int pin, int length) {
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(length);//in microseconds
-  digitalWrite(pin, LOW);
-  delay(20); 
+  closeGripper();
+  delay(2000);
+  openGripper();
+  delay(2000);
 }
 
 void openGripper(){
-  servo(gripperPin, gripper_open_pulse);
+  for(int i = 0; i<4; i++){
+    digitalWrite(gripperPin, HIGH);
+    delayMicroseconds(1700);//pulse duration in microseconds
+    digitalWrite(gripperPin, LOW);
+    delay(20);
+  }
 }
-  
+
 void closeGripper(){
-  servo(gripperPin, gripper_close_pulse);
+  for(int i = 0; i<4; i++){
+    digitalWrite(gripperPin, HIGH);
+    delayMicroseconds(1000);//pulse duration in microseconds
+    digitalWrite(gripperPin, LOW);
+    delay(20);
+  }
 }
